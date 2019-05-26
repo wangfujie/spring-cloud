4097d9e4cd0e002f8e2bfbceef82de36fe7d015d
wangfj <784727590@qq.com>
2019-05-26 16:38:40 +0800

    修改配置

--- microService-eureka-server/src/main/java/com/wangfj/MicroServiceEurekaServerApplication.java
+++ microService-eureka-server/src/main/java/com/wangfj/MicroServiceEurekaServerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer//EnableEurekaServer微服务端启动类，接受其它微服务注册进来
 public class MicroServiceEurekaServerApplication {
 
 	public static void main(String[] args) {
