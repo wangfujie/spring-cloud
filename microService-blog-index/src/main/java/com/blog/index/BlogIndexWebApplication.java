54311872cf459fce923c5fa9b5b9a0d1d39be5ea
wangfj <wangfj@chinawiserv.com>
2019-07-23 18:09:59 +0800

    修改

--- microService-blog-index/src/main/java/com/blog/index/BlogIndexWebApplication.java
+++ microService-blog-index/src/main/java/com/blog/index/BlogIndexWebApplication.java
@@ -2,11 +2,15 @@ package com.blog.index;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author wangfujie
  */
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 public class BlogIndexWebApplication {
 
 	public static void main(String[] args) {
